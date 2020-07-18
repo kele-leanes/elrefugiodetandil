7c87e9dffc9b8c5bda36fb34b7f0b4c7efcb9a56
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2020-07-18 11:45:44 -0300

    video compresion

--- src/components/VideoContainer/index.js
+++ src/components/VideoContainer/index.js
@@ -1,38 +1,16 @@
-import React, { Component } from 'react'
-import vertical from '../../videos/Vertical.MOV'
-import landscape from '../../videos/Refugio.MOV'
+import React from 'react'
+import video from '../../videos/Refugio.mp4'
 import './index.css'
 
 
 
-class VideoContainer extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            verticalVideo: null
-        }
-    }
-    
-    componentWillMount() {
-        
-        let orientation = (window.screen.orientation || {}).type || window.screen.mozOrientation || window.screen.msOrientation;
-        
-        if (orientation === "landscape-primary" || orientation === "landscape-secondary") {
-            this.setState({ verticalVideo: false });
-        } else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
-            this.setState({ verticalVideo: true });
-        } else if (orientation === undefined) {
-        console.log("The orientation API isn't supported in this browser :("); 
-        }
-    }
+function VideoContainer() {
 
-    render() {
         return (
             <div className='video-container'>
-                { this.state.verticalVideo ? <video className='video-wrapper' src={ vertical }  autoPlay loop muted ></video> : <video className='video-wrapper' src={ landscape }  autoPlay loop muted></video> }         
+                <video className='video-wrapper' src={ video }  autoPlay loop muted ></video>      
             </div>
         );
-    }
 }
 
 export default VideoContainer;

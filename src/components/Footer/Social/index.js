fae34a71027ffacc51c08525df9685927032ad34
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2020-07-30 14:19:00 -0300

    responsive footer

--- src/components/Footer/Social/index.js
+++ src/components/Footer/Social/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Media from 'react-media'
 
 function Social() {
     return(
+        <Media queries={{ small: { maxWidth: 899 } }}>
+                    {matches => matches.small ?
         <div className='social'>
             <a href='https://www.facebook.com/elrefugiodetandil/' target='_blank' rel="noopener noreferrer"><i className="fab fa-facebook"></i></a>
             <a href='https://www.instagram.com/elrefugiodetandil/' target='_blank' rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
             <a href='https://wa.me/5492494209020' target='_blank' rel="noopener noreferrer"><i className="fab fa-whatsapp"></i></a>
+        </div> : 
+            <div className='social'>
+            <a href='https://www.facebook.com/elrefugiodetandil/' target='_blank' rel="noopener noreferrer"><i className="fab fa-facebook"></i><span>Facebook</span></a>
+            <a href='https://www.instagram.com/elrefugiodetandil/' target='_blank' rel="noopener noreferrer"><i className="fab fa-instagram"></i><span>Instagram</span></a>
+            <a href='https://wa.me/5492494209020' target='_blank' rel="noopener noreferrer"><i className="fab fa-whatsapp"></i><span>Whatsapp</span></a>
         </div>
+        }
+        </Media>
     )
 }
 
